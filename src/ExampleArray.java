0fff95d842938aafbd6a6e0f29b5f6083674f209
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-03 19:09:35 -0300

    Activity 21 Description.

---
+++ src/ExampleArray.java
@@ -0,0 +1,21 @@
+/* Page 243.
+ *
+ * Activity 21: Finding the Smallest Number in an Array
+ * 1. Create a class called ExampleArray and create the main method.
+ *
+ * 2. Create an array made up of 20 floating points as follows:
+ * 14, 28, 15, 89, 46, 25, 94, 33, 82, 11, 37, 59, 68, 27, 16, 45, 24, 33, 72, 51
+ *
+ * 3. Create a for-each loop through the array and find the minimum element in the array.
+ *
+ * 4. Print out the minimum float.
+ *
+ * The solution for this activity can be found on page 335.
+ *
+ */
+
+public class ExampleArray {
+    public static void main(String[] args) {
+        
+    }
+}
